d8b0b22e5959669db8c62c9e643eede9c1c5eca1
adrianpraja <adrian@databank.ro>
2020-02-17 11:58:45 +0200

    +deleteBucket

---
+++ src/deleteBucket.js
@@ -0,0 +1,12 @@
+
+/*
+event
+	account_id
+	name
+*/
+
+module.exports = function( event, response ) {
+	console.log('[s3] deleteBucket', event.name )
+	database.buckets.del( event.account_id + ' > ' + event.name )
+	response.end()
+}
